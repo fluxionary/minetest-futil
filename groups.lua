30cc64822b1b76aa2c84fd705e1db1628bf199d9
flux <25628292+fluxionary@users.noreply.github.com>
2022-09-05 12:05:47 -0700

    get_items_with_group

--- groups.lua
+++ groups.lua
@@ -23,3 +23,35 @@ function futil.remove_groups(itemstring, ...)
     end
     minetest.override_item(itemstring, {groups = groups})
 end
+
+function futil.get_items_with_group(group)
+	local items_by_group = futil.items_by_group
+
+	if items_by_group then
+		return items_by_group[group] or {}
+	end
+
+	local items = {}
+
+	for item in pairs(minetest.registered_items) do
+		if minetest.get_item_group(item, group) > 0 then
+			table.insert(items, item)
+		end
+	end
+
+	return items
+end
+
+minetest.register_on_mods_loaded(function()
+	local items_by_group = {}
+
+	for item, def in pairs(minetest.registered_items) do
+		for group in ipairs(def.groups or {}) do
+			local items = items_by_group[group] or {}
+			table.insert(items, item)
+			items_by_group[group] = items
+		end
+	end
+
+	futil.items_by_group = items_by_group
+end)
