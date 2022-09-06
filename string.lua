05c8d5f22bf1ba7a5230fe0264949fcb1f408a24
flux <25628292+fluxionary@users.noreply.github.com>
2022-09-06 14:38:48 -0700

    add lc_cmp

--- string.lua
+++ string.lua
@@ -1,8 +1,11 @@
-
 function futil.truncate(s, max_length)
 	if s:len() > max_length then
 		return s:sub(1, max_length - 3) .. "..."
 	else
 		return s
 	end
+end
+
+function futil.lc_cmp(a, b)
+    return a:lower() < b:lower()
 end
