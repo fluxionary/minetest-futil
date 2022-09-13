497006d9a9471d2793700e422bac1531613294c9
flux <25628292+fluxionary@users.noreply.github.com>
2022-09-12 17:58:48 -0700

    resolve itemstrings, not just item 'names'

--- items.lua
+++ items.lua
@@ -1,11 +1,15 @@
 function futil.resolve_item(item)
-	local alias = minetest.registered_aliases[item]
+	local item_stack = ItemStack(item)
+	local name = item_stack:get_name()
+
+	local alias = minetest.registered_aliases[name]
 	while alias do
-		item = alias
-		alias = minetest.registered_aliases[item]
+		name = alias
+		alias = minetest.registered_aliases[name]
 	end
 
-	if minetest.registered_items[item] then
-		return item
+	if minetest.registered_items[name] then
+		item_stack:set_name(name)
+		return item_stack:to_string()
 	end
 end
