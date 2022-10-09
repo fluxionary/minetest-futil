374b96be7f9241742cf8714bf6c1db3d2d2afc43
flux <25628292+fluxionary@users.noreply.github.com>
2022-10-09 15:50:35 -0700

    fix usage

--- minetest/serialization.lua
+++ minetest/serialization.lua
@@ -8,7 +8,7 @@ function futil.serialize(x)
 
 	elseif type(x) == "table" then
 		local parts = {}
-		for k, v in futil.pairs_by_key(x) do
+		for k, v in table.pairs_by_key(x) do
 			table.insert(parts, ("[%s] = %s"):format(futil.serialize(k), futil.serialize(v)))
 		end
 		return ("{%s}"):format(table.concat(parts, ", "))
