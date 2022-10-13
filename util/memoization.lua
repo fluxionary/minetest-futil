ff4cf6e56a3712108999a9b421c8e4eaa6a9cf98
flux <25628292+fluxionary@users.noreply.github.com>
2022-10-12 18:41:02 -0700

    add mod_storage backed memoization

--- util/memoization.lua
+++ util/memoization.lua
@@ -1,14 +1,31 @@
-function futil.memoize1(f)
+function futil.memoize1(func)
 	local memo = {}
 	return function(arg)
 		if arg == nil then
-			return f(arg)
+			return func(arg)
 		end
 		local rv = memo[arg]
 
 		if not rv then
-			rv = f(arg)
+			rv = func(arg)
 			memo[arg] = rv
+		end
+
+		return rv
+	end
+end
+
+local mod_storage = futil.mod_storage
+
+function futil.memoize1ms(key, func)
+	key = ("%%s:%s:memoize"):format(key)
+	return function(arg)
+		local key_key = key:format(tostring(arg))
+		local rv = mod_storage:get(key_key)
+
+		if not rv then
+			rv = func(arg)
+			mod_storage:set_string(key_key, tostring(rv))
 		end
 
 		return rv
