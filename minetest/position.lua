7d3c76148e75fc1880dde7055feca5e209df4998
flux <25628292+fluxionary@users.noreply.github.com>
2022-10-14 12:35:16 -0700

    minor optimizations

--- minetest/position.lua
+++ minetest/position.lua
@@ -12,20 +12,25 @@ local mapgen_limit_max = (mapgen_limit_b + 1) * mapblock_size - 1
 local map_min_i = mapgen_limit_min + (mapblock_size * chunksize)
 local map_max_i = mapgen_limit_max - (mapblock_size * chunksize)
 
-local map_min_p = vector.new(map_min_i, map_min_i, map_min_i)
-local map_max_p = vector.new(map_max_i, map_max_i, map_max_i)
+local v_add = vector.add
+local v_new = vector.new
+local v_sort = vector.sort
+local v_sub = vector.subtract
+
+local map_min_p = v_new(map_min_i, map_min_i, map_min_i)
+local map_max_p = v_new(map_max_i, map_max_i, map_max_i)
 
 function futil.get_bounds(pos, radius)
-	return vector.subtract(pos, radius), vector.add(pos, radius)
+	return v_sub(pos, radius), v_add(pos, radius)
 end
 
 function futil.get_blockpos(pos)
-    return vector.new(math.floor(pos.x / 16), math.floor(pos.y / 16), math.floor(pos.z / 16))
+    return v_new(math.floor(pos.x / 16), math.floor(pos.y / 16), math.floor(pos.z / 16))
 end
 
 function futil.get_block_bounds(blockpos)
-    return vector.new(blockpos.x * 16, blockpos.y * 16, blockpos.z * 16),
-    vector.new(blockpos.x * 16 + 15, blockpos.y * 16 + 15, blockpos.z * 16 + 15)
+    return v_new(blockpos.x * 16, blockpos.y * 16, blockpos.z * 16),
+    v_new(blockpos.x * 16 + 15, blockpos.y * 16 + 15, blockpos.z * 16 + 15)
 end
 
 function futil.formspec_pos(pos)
@@ -33,6 +38,7 @@ function futil.formspec_pos(pos)
 end
 
 function futil.iterate_area(minp, maxp)
+	minp, maxp = v_sort(minp, maxp)
 	local cur = table.copy(minp)
 	cur.x = cur.x - 1
 	return function()
@@ -61,11 +67,12 @@ function futil.iterate_volume(pos, radius)
 	return futil.iterate_area(futil.get_bounds(pos, radius))
 end
 
-function futil.is_pos_in_bounds(min_p, pos, max_p)
+function futil.is_pos_in_bounds(minp, pos, maxp)
+	minp, maxp = v_sort(minp, maxp)
 	return (
-		in_bounds(min_p.x, pos.x, max_p.x) and
-		in_bounds(min_p.y, pos.y, max_p.y) and
-		in_bounds(min_p.z, pos.z, max_p.z)
+		in_bounds(minp.x, pos.x, maxp.x) and
+		in_bounds(minp.y, pos.y, maxp.y) and
+		in_bounds(minp.z, pos.z, maxp.z)
 	)
 end
 
@@ -78,7 +85,7 @@ function futil.is_inside_world_bounds(pos)
 end
 
 function futil.bound_position_to_world(pos)
-	return vector.new(
+	return v_new(
 		bound(map_min_i, pos.x, map_max_i),
 		bound(map_min_i, pos.y, map_max_i),
 		bound(map_min_i, pos.z, map_max_i)
