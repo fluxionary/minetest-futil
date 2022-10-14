2c08c61e0ce171b0f2744b319fc4b2a8fff37ce4
flux <25628292+fluxionary@users.noreply.github.com>
2022-10-14 16:55:02 -0700

    autosyncratic memes

--- minetest/time.lua
+++ minetest/time.lua
@@ -1,4 +1,11 @@
-function futil.wait(n)
-    local wait_until = minetest.get_us_time() + n
-    while minetest.get_us_time() < wait_until do end
+function futil.wait(us)
+    local wait_until = minetest.get_us_time() + us
+	local get_us_time = minetest.get_us_time
+    while get_us_time() < wait_until do
+		-- the NOTHING function does nothing.
+	end
+end
+
+function futil.format_utc(timestamp)
+
 end
