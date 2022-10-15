47a86b78057e2a71ac0e4b713970ac2f21bf9ed6
flux <25628292+fluxionary@users.noreply.github.com>
2022-10-15 12:02:58 -0700

    split_region_by_mapblock

--- minetest/vector.lua
+++ minetest/vector.lua
@@ -1,5 +1,7 @@
 -- luacheck: globals vector
 
+local m_min = math.min
+local m_max = math.max
 local m_floor = math.floor
 
 local in_bounds = math.in_bounds
@@ -100,4 +102,38 @@ end
 function vector.volume(pos1, pos2)
 	local minp, maxp = v_sort(pos1, pos2)
 	return (maxp.x - minp.x + 1) * (maxp.y - minp.y + 1) * (maxp.z - minp.z + 1)
+end
+
+function futil.split_region_by_mapblock(pos1, pos2, num_blocks)
+	local chunk_size = 16 * (num_blocks or 1)
+	local chunk_span = chunk_size - 1
+
+	pos1, pos2 = v_sort(pos1, pos2)
+
+	local x1 = pos1.x - (pos1.x % chunk_size)
+	local x2 = pos2.x - (pos2.x % chunk_size) + chunk_span
+	local y1 = pos1.y - (pos1.y % chunk_size)
+	local y2 = pos2.y - (pos2.y % chunk_size) + chunk_span
+	local z1 = pos1.z - (pos1.z % chunk_size)
+	local z2 = pos2.z - (pos2.z % chunk_size) + chunk_span
+
+    local chunks = {}
+    for x = x1, x2, chunk_size do
+		local x_min = m_max(pos1.x, x)
+		local x_max = m_min(pos2.x, x + chunk_span)
+
+        for y = y1, y2, chunk_size do
+			local y_min = m_max(pos1.y, y)
+			local y_max = m_min(pos2.y, y + chunk_span)
+
+            for z = z1, z2, chunk_size do
+				local z_min = m_max(pos1.z, z)
+				local z_max = m_min(pos2.z, z + chunk_span)
+
+                table.insert(chunks, {v_new(x_min, y_min, z_min), v_new(x_max, y_max, z_max)})
+            end
+        end
+    end
+
+    return chunks
 end
