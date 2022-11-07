857839b55a3906c438244f390ae3383e0f52117d
flux <25628292+fluxionary@users.noreply.github.com>
2022-11-07 08:12:06 -0800

    make DefaultTable actually work

--- collections/default_table.lua
+++ collections/default_table.lua
@@ -1,20 +1,33 @@
-local DefaultTable = futil.class1()
+--local DefaultTable = futil.class1()
+--
+--function DefaultTable:_init(initializer)
+--	-- TODO should check that initializer is executable
+--	rawset(self, "_initializer", initializer)
+--	rawset(self, "_contents", {})
+--end
+--
+--function DefaultTable:__index(key)
+--	local contents = rawget(self, "_contents")
+--	local value = contents[key]
+--
+--	if value == nil then
+--		local initializer = rawget(self, "_initializer")
+--		value = initializer(key)
+--		contents[key] = value
+--	end
+--
+--	return value
+--end
+--
+--function DefaultTable:__newindex(key, value)
+--	local contents = rawget(self, "_contents")
+--	contents[key] = value
+--end
 
-function DefaultTable:_init(initializer)
-	-- TODO should check that initializer is executable
-	self._initializer = initializer
-	self._contents = {}
-end
-
-function DefaultTable:__index(key)
-	local value = self._contents[key]
-	if not value then
-		value = self._initializer()
-		self._contents[key] = value
-	end
-	return value
-end
-
-function DefaultTable:__newindex(key, value)
-	self._contents[key] = value
+function futil.DefaultTable(initializer)
+	return setmetatable({}, {__index = function(t, k)
+		local v = initializer(k)
+		t[k] = v
+		return v
+	end})
 end
