f069ef97e34d88775d0fc061b044478b748c23a3
flux <25628292+fluxionary@users.noreply.github.com>
2022-11-12 12:37:56 -0800

    remove dead code

--- collections/default_table.lua
+++ collections/default_table.lua
@@ -1,29 +1,3 @@
---local DefaultTable = futil.class1()
---
---function DefaultTable:_init(initializer)
---	-- TODO should check that initializer is executable
---	rawset(self, "_initializer", initializer)
---	rawset(self, "_contents", {})
---end
---
---function DefaultTable:__index(key)
---	local contents = rawget(self, "_contents")
---	local value = contents[key]
---
---	if value == nil then
---		local initializer = rawget(self, "_initializer")
---		value = initializer(key)
---		contents[key] = value
---	end
---
---	return value
---end
---
---function DefaultTable:__newindex(key, value)
---	local contents = rawget(self, "_contents")
---	contents[key] = value
---end
-
 function futil.DefaultTable(initializer)
 	return setmetatable({}, {__index = function(t, k)
 		local v = initializer(k)
