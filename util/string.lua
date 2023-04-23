11021037506cab837ad66101e001b67776fe2885
flux <25628292+fluxionary@users.noreply.github.com>
2023-04-23 16:21:23 -0700

    string.startswith

--- util/string.lua
+++ util/string.lua
@@ -13,3 +13,7 @@ end
 function futil.string.lc_cmp(a, b)
 	return a:lower() < b:lower()
 end
+
+function futil.string.startswith(s, start, start_i, end_i)
+	return s:sub(start_i or 0, end_i or #s):sub(1, #start) == start
+end
