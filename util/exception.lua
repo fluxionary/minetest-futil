b9554fd533955f4f6c16e8025e59227265d727e6
flux <25628292+fluxionary@users.noreply.github.com>
2023-05-09 09:57:36 -0700

    check_call: allow specifying a custom error callback

--- util/exception.lua
+++ util/exception.lua
@@ -1,4 +1,4 @@
-function futil.check_call(func, rv_on_fail)
+function futil.check_call(func, rv_on_fail, error_callback)
 	-- wrap a function w/ logic to avoid crashing the game
 	return function(...)
 		local rvs = { xpcall(func, debug.traceback, ...) }
@@ -7,7 +7,17 @@ function futil.check_call(func, rv_on_fail)
 			table.remove(rvs, 1)
 			return unpack(rvs)
 		else
-			futil.log("error", "(check_call): %s args: %s out: %s", dump(debug.getinfo(func)), dump({ ... }), rvs[2])
+			if error_callback then
+				error_callback(debug.getinfo(func), { ... }, rvs[2])
+			else
+				futil.log(
+					"error",
+					"(check_call): %s args: %s out: %s",
+					dump(debug.getinfo(func)),
+					dump({ ... }),
+					rvs[2]
+				)
+			end
 			return rv_on_fail
 		end
 	end
