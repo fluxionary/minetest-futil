72784b2ed601c5e9c3d43d7deee464d239fbe779
flux <25628292+fluxionary@users.noreply.github.com>
2023-07-14 11:26:56 -0700

    random choice, random sample

--- util/random.lua
+++ util/random.lua
@@ -1,3 +1,5 @@
+local f = string.format
+
 futil.random = {}
 
 function futil.random.choice(t, random)
@@ -18,4 +20,27 @@ function futil.random.weighted_choice(t, random)
 	local value = random() * breaks[#breaks]
 	i = futil.bisect.right(breaks, value)
 	return elements[i]
+end
+
+function futil.random.choice(t, random)
+	assert(#t > 0, "cannot get choice from an empty table")
+	random = random or math.random
+	return t[random(#t)]
+end
+
+-- https://stats.stackexchange.com/questions/569647/
+function futil.random.sample(t, k, random)
+	assert(k <= #t, f("cannot sample %i items from a set of size %i", k, #t))
+	random = random or math.random
+	local sample = {}
+	for i = 1, k do
+		sample[i] = t[k]
+	end
+	for j = k + 1, #t do
+		if random() < k / j then
+			sample[random(1, k)] = t[j]
+		end
+	end
+
+	return sample
 end
