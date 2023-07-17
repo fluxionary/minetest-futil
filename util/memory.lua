a63adb47a72d9e664133b95822d6f7808f33c24c
flux <25628292+fluxionary@users.noreply.github.com>
2023-07-17 15:10:28 -0700

    improve accuracy of memory estimator (maybe)

--- util/memory.lua
+++ util/memory.lua
@@ -3,39 +3,39 @@
 
 local function estimate_memory_usage(thing, seen)
 	local typ = type(thing)
-	if typ == "nil" or typ == "boolean" then
+	if typ == "nil" then
 		return 0
+	end
+
+	seen = seen or {}
+	if seen[thing] then
+		return 0
+	end
+	seen[thing] = true
+
+	if typ == "boolean" then
+		return 4
 	elseif typ == "number" then
-		return 8 -- this is probably larger
+		return 8 -- this is probably larger?
 	elseif typ == "string" then
-		return 24 + typ:len()
+		return 25 + typ:len()
 	elseif typ == "function" then
-		-- TODO: we can calculate the usage of closures, but that's complicated
-		return 20
+		-- TODO: we can calculate the usage of upvalues, but that's complicated
+		return 40
 	elseif typ == "userdata" then
 		return 0 -- this is probably larger
 	elseif typ == "thread" then
-		return 0 -- this is probably larger
+		return 1224 -- this is probably larger
 	elseif typ == "table" then
-		seen = seen or {}
-		if seen[thing] then
-			return 0
-		end
-		seen[thing] = true
-		if #thing > 0 then
-			-- assume list
-			local size = 40 + 16 * #thing
-			for i = 1, #thing do
-				size = size + estimate_memory_usage(thing[i], seen)
-			end
-			return size
-		else
-			local size = 40
-			for k, v in pairs(thing) do
+		local size = 64
+		for k, v in pairs(thing) do
+			if type(k) == "number" then
+				size = size + 16 + estimate_memory_usage(v, seen)
+			else
 				size = size + 40 + estimate_memory_usage(k, seen) + estimate_memory_usage(v, seen)
 			end
-			return size
 		end
+		return size
 	else
 		futil.log("warning", "estimate_memory_usage: unknown type %s", typ)
 		return 0 -- ????
