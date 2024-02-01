ab2bf879e114e82b771255bed9530f8afe575057
flux <25628292+fluxionary@users.noreply.github.com>
2024-02-01 12:32:28 -0800

    add the 'closest point to two lines' algorithm

--- minetest/vector.lua
+++ minetest/vector.lua
@@ -364,3 +364,32 @@ function futil.vector.compose_rotations(rot1, rot2)
 	local m2 = futil.vector.rotation_to_matrix(rot2)
 	return futil.vector.matrix_to_rotation(futil.matrix.multiply(m1, m2))
 end
+
+-- https://palitri.com/vault/stuff/maths/Rays%20closest%20point.pdf
+-- this was originally part of the ballistics mod but i don't need it there anymore
+function futil.vector.closest_point_to_two_lines(last_pos, last_vel, cur_pos, cur_vel, threshold)
+	threshold = threshold or 0.0001 -- if certain values are too close to 0, the results will not be good
+	local a = cur_vel
+	local b = last_vel
+	local a2 = a:dot(a)
+	if a2 < threshold then
+		return
+	end
+	local b2 = b:dot(b)
+	if b2 < threshold then
+		return
+	end
+	local ab = a:dot(b)
+	local denom = (a2 * b2) - (ab * ab)
+	if denom < threshold then
+		return
+	end
+	local A = cur_pos
+	local B = last_pos
+	local c = last_pos - cur_pos
+	local bc = b:dot(c)
+	local ac = a:dot(c)
+	local D = A + a * ((ac * b2 - ab * bc) / denom)
+	local E = B + b * ((ab * ac - bc * a2) / denom)
+	return (D + E) / 2
+end
