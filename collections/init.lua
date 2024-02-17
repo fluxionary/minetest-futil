404f9c68627a41883823f477aa3165baf83cb7a8
flux <25628292+fluxionary@users.noreply.github.com>
2024-02-17 14:35:09 -0800

    simple adjacency-set graph

--- collections/init.lua
+++ collections/init.lua
@@ -4,3 +4,4 @@ futil.dofile("collections", "deque")
 futil.dofile("collections", "pairing_heap")
 futil.dofile("collections", "point_search_tree")
 futil.dofile("collections", "set")
+futil.dofile("collections", "sparse_graph") -- requires default_table and set
